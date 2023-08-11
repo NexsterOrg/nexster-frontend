93ea0669a3122845b547b1e9519d308c5ec29398
NamalSanjaya <namalsanjaya23@gmail.com>
2023-08-12 02:22:19 +0530

    Add redirect path

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,8 +1,9 @@
 import React, {useState, useEffect} from "react"
 import {List, ListItem, Typography} from '@mui/material'
+import { useNavigate } from 'react-router-dom';
 
 import TimelinePost from  "./media/post"
-import { fetchData, MkPostsFetchUrl } from "../apis/fetch"
+import { fetchData, MkPostsFetchUrl, UnAuthorizedError } from "../apis/fetch"
 import Base1 from "./layout/base1"
 
 /** 
@@ -16,6 +17,7 @@ const gap = Math.floor(postCnt/2)*700  // To fetch 5 posts. gap should be (5//2)
 let limit = gap
 
 function ListTimelinePosts({userId}){
+    const navigate = useNavigate();
     // TODO:
     // data array should only contain unique values. But due to some uncertainty nature, this can have 
     // same value twice. check this.
@@ -32,7 +34,12 @@ function ListTimelinePosts({userId}){
               }
               addPostsInfo({preLn: data.length, data: data});
             } catch (err) {
-              console.error('Error fetching posts:', err);
+                if (err instanceof UnAuthorizedError) {
+                    // TODO: before naviagating to /login, we need to remove JWT token from local stroage()
+                    navigate('/login', { replace: true });
+                    return
+                } 
+                console.error('Error fetching posts:', err); // TODO : Remove this in production
             }
         })();
     }, [])
@@ -41,7 +48,7 @@ function ListTimelinePosts({userId}){
         const handleScroll = async () => {
             if (window.scrollY > limit) {
                 limit += gap
-                if(postsInfo.preLn == 0) {
+                if(postsInfo.preLn === 0) {
                     // All posts have been read
                     return
                 }
@@ -61,7 +68,11 @@ function ListTimelinePosts({userId}){
                         });
                     }
                 } catch (err) {
-                    console.error('Unhandled error:', err);
+                    if (err instanceof UnAuthorizedError) {  // TODO : Remove this in production
+                        navigate('/login', { replace: true });
+                        return
+                    } 
+                    console.error('Error fetching posts:', err);
                 }
             }
         };
