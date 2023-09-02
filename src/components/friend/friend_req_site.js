6ec72213657b5d84f3148fc03a15d112192c3693
NamalSanjaya <namalsanjaya23@gmail.com>
2023-09-02 23:01:33 +0530

    Add back button to go to friend section

--- src/components/friend/friend_req_site.js
+++ src/components/friend/friend_req_site.js
@@ -1,15 +1,31 @@
+import {Box, Button} from '@mui/material';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { useNavigate } from 'react-router-dom';
+
 import FriendReqsPanel from "./friend_req_panel"
 import Base1 from "../layout/base1";
+import { FriendsRoute } from '../../apis/fetch';
 
 const reqCountPerFetch = 15
 
-function SideFriendReqSite(){
+export default function FriendReqSite(){
+    const navigate = useNavigate();
 
-    return (
-        <FriendReqsPanel rootStyles={{margin: "30px"}} showButton={false} initPageNo={1} initPageSize={reqCountPerFetch} pageSize={reqCountPerFetch}/>
-    )
+    const backHandler = () => {
+        navigate(FriendsRoute);
+    }
+
+    const comp = <Box> 
+        <Button sx={styles.butn} startIcon={<ArrowBackIcon />} onClick={backHandler}>Back to Friends</Button>
+        <FriendReqsPanel rootStyles={{marginX: "30px", marginBottom: "30px", marginTop: "8px"}} showButton={false} initPageNo={1} initPageSize={reqCountPerFetch} 
+        pageSize={reqCountPerFetch}/>
+    </Box>
+
+    return <Base1 SideComponent={comp} styles={{alignItems: "flex-start"}}/>
 }
 
-export default function FriendReqSite(){
-    return <Base1 SideComponent={<SideFriendReqSite />} styles={{alignItems: "flex-start"}}/>
+const styles = {
+    butn: {
+        marginTop: "30px", marginLeft: "30px", textTransform: "none"
+    }
 }
--- src/components/friend/friend_sugg_site.js
+++ src/components/friend/friend_sugg_site.js
@@ -1,9 +1,28 @@
+import {Box, Button} from '@mui/material';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { useNavigate } from 'react-router-dom';
+
 import Base1 from "../layout/base1";
 import FriendSuggPanel from "./friends_sugg_panel"
+import { FriendsRoute } from '../../apis/fetch';
 
 const suggsSitePageSize = 18
 
 export default function FriendSuggsSite(){
-    return <Base1 SideComponent={<FriendSuggPanel rootStyles={{margin: "30px"}} showButton={false} pageSize={suggsSitePageSize}/>} 
-    styles={{alignItems: "flex-start"}}/>
+    const navigate = useNavigate();
+
+    const backHandler = () => {
+        navigate(FriendsRoute);
+    }
+    const comp = <Box> 
+        <Button sx={styles.butn} startIcon={<ArrowBackIcon />} onClick={backHandler}>Back to Friends</Button>
+        <FriendSuggPanel rootStyles={{marginX: "30px", marginBottom: "30px", marginTop: "8px"}} showButton={false} pageSize={suggsSitePageSize}/>
+    </Box>
+    return <Base1 SideComponent={comp} styles={{alignItems: "flex-start"}}/>
+}
+
+const styles = {
+    butn: {
+        marginTop: "30px", marginLeft: "30px", textTransform: "none"
+    }
 }
