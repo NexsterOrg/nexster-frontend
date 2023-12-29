d399d45b565594d6c49fb6abbf7e7089f1e9887a
NamalSanjaya <namalsanjaya23@gmail.com>
2023-12-30 00:40:43 +0530

    Add domain names

--- src/apis/fetch.js
+++ src/apis/fetch.js
@@ -1,7 +1,7 @@
 import { CleanLS } from "./store"
 
-const apiDomain = "https://20.197.22.26"  // backend nginx proxy domain
-const webDomain = "https://exp-mora-nexster.azurewebsites.net"  // front end Domain
+const apiDomain = "https://api.nexster.xyz"  // backend nginx proxy domain
+const webDomain = "https://nexster.xyz"  // front end Domain
 const token = "token"
 
 // nexster paths
